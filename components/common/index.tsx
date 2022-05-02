9e0a0283f2e915f75338b77cbd86384d11c79e5e
theNvN <thenvnsahu@gmail.com>
2022-05-02 17:02:13 +0530

    exports

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -1,3 +1,4 @@
 export { default as Link } from './Link';
 export { default as Logo } from './Logo';
 export { default as Mark } from './Mark';
+export { default as PageLoading } from './PageLoading';
