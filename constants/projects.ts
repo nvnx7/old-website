60c2c31ede64051cb469fa0588f323a0f29c88a1
theNvN <thenvnsahu@gmail.com>
2022-05-02 22:26:21 +0530

    add: project list

---
+++ constants/projects.ts
@@ -0,0 +1,37 @@
+export interface ProjectItem {
+  title: string;
+  description: string;
+  github: string;
+  tech: string[];
+}
+
+const projects: ProjectItem[] = [
+  {
+    title: 'hardhat-test-utils',
+    description:
+      'A Hardhat plugin exposing a collection of test utilities for testing smart-contracts in Hardhat projects.',
+    github: 'https://github.com/theNvN/hardhat-test-utils',
+    tech: ['typescript', 'solidity'],
+  },
+  {
+    title: 'DAO Launcher',
+    description: 'Launch your OZ-compatible DAOs on Ethereum networks within a few clicks.',
+    github: 'https://github.com/theNvN/dao-launcher',
+    tech: ['nextjs', 'hardhat', 'typescript', 'solidity', 'chakra-ui'],
+  },
+  {
+    title: 'Artista',
+    description:
+      'Native android app written in Kotlin. Style transfer photos using Deep Learning model at the edge - no internet required.',
+    github: 'https://github.com/theNvN/Artista',
+    tech: ['kotlin', 'android', 'tensorflow-lite'],
+  },
+  {
+    title: 'gaze-pointer-controller',
+    description: 'Mouse pointer controller with human eye gaze.',
+    github: 'https://github.com/theNvN/eye-gaze-pointer-controller',
+    tech: ['python3', 'intel-openvino', 'opencv'],
+  },
+];
+
+export default projects;
