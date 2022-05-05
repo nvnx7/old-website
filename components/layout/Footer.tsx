8ccf0381a575a56c79f2d869389e90807e8a95c8
theNvN <thenvnsahu@gmail.com>
2022-05-05 22:48:47 +0530

    update

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,18 +1,33 @@
-import { Footer as GrommetFooter, BoxProps, Text, Box } from 'grommet';
+import { Link, Mark } from 'components/common';
+import { SocialLinks } from 'components/sections';
+import { Footer as GrommetFooter, BoxProps, Text, Box, Paragraph } from 'grommet';
+import Column from './Column';
+import Row from './Row';
 
 const Footer: React.FC<BoxProps> = ({ ...props }) => {
   return (
-    <GrommetFooter justify="center" {...props}>
-      <Text textAlign="center" weight={900}>
-        design{' '}
-        <Text as="span" color="brand">
-          x
-        </Text>{' '}
-        dev by{' '}
-        <Text as="span" color="brand">
-          @theNvN
+    <GrommetFooter justify="between" align="center" direction="column" gap="small" {...props}>
+      <Box height="4px" width="xxsmall" background="brand" />
+      <Row width="full">
+        <SocialLinks width="small" />
+
+        <Text textAlign="center" size="small" alignSelf="center">
+          design{' '}
+          <Text as="span" color="brand" size="small">
+            x
+          </Text>{' '}
+          dev by <Mark>@theNvN</Mark>
         </Text>
-      </Text>
+
+        <Box>
+          <Paragraph size="small">
+            This website is open-source on{' '}
+            <Link text={{ size: 'small' }} href="https://github.com/theNvN/website">
+              github.
+            </Link>
+          </Paragraph>
+        </Box>
+      </Row>
     </GrommetFooter>
   );
 };
