f989a0fba65de1176451707b0ee6ead3a4368e45
theNvN <thenvnsahu@gmail.com>
2022-05-05 22:45:57 +0530

    added link

--- constants/social.ts
+++ constants/social.ts
@@ -2,6 +2,7 @@ const socialLinks = {
   twitter: 'https://twitter.com/heyNvN',
   github: 'https://github.com/theNvN',
   whatsapp: 'https://wa.me/917007524267',
+  dev: 'https://dev.to/nvn',
   email: 'mailto:thenvnsahu@gmail.com',
   linkTree: 'https://linktr.ee/thenvn',
 };
