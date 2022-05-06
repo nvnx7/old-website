052f6ee2cb335d85ad3288815c91ae8e2db2114c
theNvN <thenvnsahu@gmail.com>
2022-05-06 23:09:53 +0530

    take iconSize prop

--- components/sections/SocialLinks.tsx
+++ components/sections/SocialLinks.tsx
@@ -2,17 +2,21 @@ import { Anchor, Box, BoxTypes } from 'grommet';
 import { Github, Mail, Twitter } from 'grommet-icons';
 import socialLinks from 'constants/social';
 
-const SocialLinks: React.FC<BoxTypes> = ({ ...props }) => {
+interface ISocialLinksProps extends BoxTypes {
+  iconSize?: string;
+}
+
+const SocialLinks: React.FC<ISocialLinksProps> = ({ iconSize = 'medium', ...props }) => {
   return (
     <Box width="full" direction="row" justify="between" align="center" {...props}>
       <Anchor href={socialLinks.twitter} target="_blank">
-        <Twitter color="brand" />
+        <Twitter color="brand" size={iconSize} />
       </Anchor>
       <Anchor href={socialLinks.github} target="_blank">
-        <Github color="brand" />
+        <Github color="brand" size={iconSize} />
       </Anchor>
       <Anchor href={socialLinks.email}>
-        <Mail color="brand" />
+        <Mail color="brand" size={iconSize} />
       </Anchor>
     </Box>
   );
